f1dd4a5d03a2e4b5f611595575bbc22ed9ac5209
icwet <icwet@yandex.ru>
2020-09-21 01:32:02 +0300

    add linters and create first components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	BrowserRouter,
+} from "react-router-dom";
+import { GlobalStyle } from "components/GlobalStyle";
+import { Menu } from "components/Menu";
 
 const App = () => {
-	return <div>Hello World!</div>;
+	return (
+		<BrowserRouter>
+			<GlobalStyle />
+			<Menu>
+
+			</Menu>
+			<div>Hello world!</div>
+		</BrowserRouter>
+	);
 };
 
 export default App;
---
+++ src/components/Menu/-Starred/index.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const MenuStarred = styled.button`
+	display: block;
+`;
